496da49cd9c878b3700088a0d5380d841599e6d9
Glawian <krzysiek.liguda@gmail.com>
2021-08-31 19:08:56 +0200

    Add docker container

--- django/advertisement/views.py
+++ django/advertisement/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import decorators, response, viewsets
 
 from advertisement.models import Category, Offer
 from advertisement.serializers import CategorySerializer, OfferSerializer
@@ -7,6 +7,13 @@ from advertisement.serializers import CategorySerializer, OfferSerializer
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+    @decorators.action(detail=True, methods=['get'])
+    def offers(self, request, pk=None):
+        category = self.get_object()
+        offers = Offer.objects.filter(category=category)
+        offers_json = OfferSerializer(offers, many=True)
+        return response.Response(offers_json.data)
 
 class OfferViewSet(viewsets.ModelViewSet):
     serializer_class = OfferSerializer
